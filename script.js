bf446977b132157ebfe1839ed770eea35e286fcf
Kakashi7860 <sohelshakh91@gmail.com>
2025-11-09 12:48:43 +0530

    up

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-const heading = document.getElementById("title");
-const button = document.getElementById("btn");
-
-let clicked = false;
-button.addEventListener("click",()=>{
-    if(!clicked){
-        heading.textContent = "you clicked the button";
-        heading.style.color = "blue";
-        clicked = true;
-    }else{
-        heading.textContent="you clicked the button";
-        heading.style.color = "green";
-        clicked = false;
-    }
-});
-
-
+function changeColor() {
+    const colors = ['#FF6B6B', '#4ECDC4', '#45B7D1', '#FFA07A', '#98D8C8', '#F7DC6F', '#BB8FCE', '#85C1E2', '#F8B195', '#C06C84'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+}
