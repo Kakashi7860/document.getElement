d4e720f40cb8395d3731830c499a39381ac9e434
Kakashi7860 <sohelshakh91@gmail.com>
2025-11-09 12:26:59 +0530

    up

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const heading = document.querySelector("#title");
-console.log(heading.textContent);
+const heading = document.getElementById("title");
+const button = document.getElementById("btn");
+
+let clicked = false;
+button.addEventListener("click",()=>{
+    if(!clicked){
+        heading.textContent = "you clicked the button";
+        heading.style.color = "blue";
+        clicked = true;
+    }else{
+        heading.textContent="you clicked the button";
+        heading.style.color = "green";
+        clicked = false;
+    }
+});
+
+
